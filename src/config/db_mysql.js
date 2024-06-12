b7c135fbeb1480ee2ca6d784e365975f43a88b48
sila1404 <silamany1404@gmail.com>
2024-06-12 19:35:16 +0700

    chagne the database from env to using string

--- src/config/db_mysql.js
+++ src/config/db_mysql.js
@@ -5,7 +5,7 @@ const conn = mysql.createConnection({
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MSQL_DATABASE,
+  database: "Nodejs_Course",
 });
 
 conn.connect((err) => {
