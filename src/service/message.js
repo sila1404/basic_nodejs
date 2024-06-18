d053fa973c8a0839df9ff0e37cfef14335fffe10
sila1404 <silamany1404@gmail.com>
2024-06-18 10:25:41 +0700

    feat: create message for error and success response

--- src/service/message.js
+++ src/service/message.js
@@ -3,3 +3,25 @@ export const Role = {
   admin: "admin",
   owner: "owner",
 };
+
+// EMessage = Error Message
+export const EMessage = {
+  Server: "Error Server Internal",
+  NotFound: "Not Found",
+  Already: "Already",
+  PleaseInput: "Please Input: ",
+  InsertError: "Error Insert",
+  UpdateError: "Error Update",
+  DeleteError: "Error Delete",
+};
+
+// SMessage = Success Message
+export const SMessage = {
+  GetOne: "Get One Success",
+  GetAll: "Get All Success",
+  Login: "Login Success",
+  Register: "Register Success",
+  Insert: "Insert Success",
+  Update: "Update Success",
+  Delete: "Delete Success",
+};
