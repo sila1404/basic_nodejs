12777f24568f424b02158ee956bbb7d4a532512a
sila1404 <silamany1404@gmail.com>
2024-06-18 10:27:42 +0700

    feat: route for user login

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import UserController from "../controller/user.controller.js";
 const router = express.Router();
 
 // ---------- user ----------
+router.post("/user/login",UserController.login);
 router.post("/user/register", UserController.register);
 
 export default router
