05c6b511b924656bd89e0a6ffe596e926b06523d
sila1404 <silamany1404@gmail.com>
2024-06-18 10:46:00 +0700

    feat: create controller to handle login route

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,12 +1,60 @@
 import { validateData } from "../service/validate.js";
 import { v4 as uuidv4 } from "uuid";
 import conn from "../config/db_mysql.js";
-import { Role } from "../service/message.js";
-import { SendCreate, SendError400, SendError500 } from "../service/response.js";
+import { EMessage, Role, SMessage } from "../service/message.js";
+import {
+  SendCreate,
+  SendError400,
+  SendError404,
+  SendError500,
+  SendSuccess,
+} from "../service/response.js";
 import CryptoJS from "crypto-js";
 import { SECRET_KEY } from "../config/config.js";
+import { Decrypts, GenerateToken } from "../service/service.js";
 
 export default class UserController {
+  static async login(req, res) {
+    try {
+      const { email, password } = req.body;
+
+      const validate = await validateData({ email, password });
+      if (validate.length > 0) {
+        return SendError400(res, EMessage.PleaseInput + validate.join(","));
+      }
+
+      const checkEmail = "select * from user where email=?";
+
+      conn.query(checkEmail, email, async (err, result) => {
+        if (err) return SendError404(res, EMessage.NotFound, err);
+
+        if (!result[0]) return SendError404(res, EMessage.NotFound, err);
+
+        const decryptPassword = await Decrypts(result[0]["password"]);
+
+        if (password != decryptPassword) {
+          return SendError400(res, "Password not match");
+        }
+
+        const data = {
+          id: result[0]["uuid"],
+          role: result[0]["role"],
+        };
+
+        const token = await GenerateToken(data);
+
+        const newData = Object.assign(
+          JSON.parse(JSON.stringify(result[0])),
+          JSON.parse(JSON.stringify(token))
+        );
+
+        return SendSuccess(res, SMessage.Login, newData);
+      });
+    } catch (error) {
+      return SendError500(res, EMessage.Server, error);
+    }
+  }
+
   static async register(req, res) {
     try {
       // Get data from request body
@@ -22,7 +70,6 @@ export default class UserController {
         return SendError400(res, `Please input: ${validate.join(", ")}`);
       }
 
-      
       const uuid = uuidv4();
       const mysql =
         "INSERT INTO user (uuid, username, email, phoneNumber, password, role, createdAt, updatedAt) VALUES (? ,? ,? ,? ,? ,? ,? ,?);";
@@ -32,7 +79,10 @@ export default class UserController {
         .replace(/\..+/, "");
 
       // Hash password before insert to table
-      const generatePassword = CryptoJS.AES.encrypt(password, SECRET_KEY).toString()
+      const generatePassword = CryptoJS.AES.encrypt(
+        password,
+        SECRET_KEY
+      ).toString();
 
       // Insert data to table
       conn.query(
