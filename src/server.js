fb3151311cb9d086fbbc1e2a1c48164780d4aeea
sila1404 <silamany1404@gmail.com>
2024-06-24 19:56:25 +0700

    using express-fileupload

--- src/server.js
+++ src/server.js
@@ -5,12 +5,14 @@ import "./config/db_mysql.js";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { PORT } from "./config/config.js";
+import fileUpload from "express-fileupload";
 
 import router from "./router/index.js";
 
 const app = express();
 
 app.use(cors());
+app.use(fileUpload())
 app.use(bodyParser.json({ extended: true }));
 app.use(
   bodyParser.urlencoded({ extended: true, limit: "500mb", parameterLimit: 500 })
